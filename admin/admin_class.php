d945251b9da86257e8116898c7b9c1c1802ac2de
karlmikelpecson <karlmikelpecson13@gmail.com>
2024-12-04 09:43:14 +0800

    add edit/save on users

--- admin/admin_class.php
+++ admin/admin_class.php
@@ -15,41 +15,41 @@ Class Action {
 	    ob_end_flush();
 	}
 
-	// function login(){
-		
-	// 		extract($_POST);		
-	// 		$qry = $this->db->query("SELECT * FROM users where username = '".$username."' and password = '".md5($password)."' ");
-	// 		if($qry->num_rows > 0){
-	// 			foreach ($qry->fetch_array() as $key => $value) {
-	// 				if($key != 'passwors' && !is_numeric($key))
-	// 					$_SESSION['login_'.$key] = $value;
-	// 			}
-	// 			if($_SESSION['login_type'] != 1){
-	// 				foreach ($_SESSION as $key => $value) {
-	// 					unset($_SESSION[$key]);
-	// 				}
-	// 				return 2 ;
-	// 				exit;
-	// 			}
-	// 				return 1;
-	// 		}else{
-	// 			return 3;
-	// 		}
-	// }
-
 	function login(){
-		extract($_POST);
-		$qry = $this->db->query("SELECT * FROM users where username = '".$username."' and password = '".$password."' ");
-		if($qry->num_rows > 0){
-			foreach ($qry->fetch_array() as $key => $value) {
-				if($key != 'passwors' && !is_numeric($key))
-					$_SESSION['login_'.$key] = $value;
+		
+			extract($_POST);		
+			$qry = $this->db->query("SELECT * FROM users where username = '".$username."' and password = '".md5($password)."' ");
+			if($qry->num_rows > 0){
+				foreach ($qry->fetch_array() as $key => $value) {
+					if($key != 'passwors' && !is_numeric($key))
+						$_SESSION['login_'.$key] = $value;
+				}
+				if($_SESSION['login_type'] != 1){
+					foreach ($_SESSION as $key => $value) {
+						unset($_SESSION[$key]);
+					}
+					return 2 ;
+					exit;
+				}
+					return 1;
+			}else{
+				return 3;
 			}
-				return 1;
-		}else{
-			return 3;
-		}
 	}
+
+	// function login(){
+	// 	extract($_POST);
+	// 	$qry = $this->db->query("SELECT * FROM users where username = '".$username."' and password = '".$password."' ");
+	// 	if($qry->num_rows > 0){
+	// 		foreach ($qry->fetch_array() as $key => $value) {
+	// 			if($key != 'passwors' && !is_numeric($key))
+	// 				$_SESSION['login_'.$key] = $value;
+	// 		}
+	// 			return 1;
+	// 	}else{
+	// 		return 3;
+	// 	}
+	// }
 	
 	function login2(){
 		
@@ -83,6 +83,7 @@ Class Action {
 			return 3;
 		}
 	}
+	
 	function logout(){
 		session_destroy();
 		foreach ($_SESSION as $key => $value) {
@@ -98,36 +99,21 @@ Class Action {
 		header("location:../index.php");
 	}
 
-	// function save_user(){
-	// 	extract($_POST);
-	// 	$data = " name = '$name' ";
-	// 	$data .= ", username = '$username' ";
-	// 	if(!empty($password))
-	// 	$data .= ", password = '".md5($password)."' ";
-	// 	$data .= ", type = '$type' ";
-	// 	if($type == 1)
-	// 		$establishment_id = 0;
-	// 	$data .= ", establishment_id = '$establishment_id' ";
-	// 	$chk = $this->db->query("Select * from users where username = '$username' and id !='$id' ")->num_rows;
-	// 	if($chk > 0){
-	// 		return 2;
-	// 		exit;
-	// 	}
-	// 	if(empty($id)){
-	// 		$save = $this->db->query("INSERT INTO users set ".$data);
-	// 	}else{
-	// 		$save = $this->db->query("UPDATE users set ".$data." where id = ".$id);
-	// 	}
-	// 	if($save){
-	// 		return 1;
-	// 	}
-	// }
 	function save_user(){
 		extract($_POST);
 		$data = " name = '$name' ";
 		$data .= ", username = '$username' ";
-		$data .= ", password = '$password' ";
+		if(!empty($password))
+		$data .= ", password = '".md5($password)."' ";
 		$data .= ", type = '$type' ";
+		if($type == 1)
+			$establishment_id = 0;
+		$data .= ", establishment_id = '$establishment_id' ";
+		$chk = $this->db->query("Select * from users where username = '$username' and id !='$id' ")->num_rows;
+		if($chk > 0){
+			return 2;
+			exit;
+		}
 		if(empty($id)){
 			$save = $this->db->query("INSERT INTO users set ".$data);
 		}else{
@@ -137,6 +123,22 @@ Class Action {
 			return 1;
 		}
 	}
+
+	// function save_user(){
+	// 	extract($_POST);
+	// 	$data = " name = '$name' ";
+	// 	$data .= ", username = '$username' ";
+	// 	$data .= ", password = '$password' ";
+	// 	$data .= ", type = '$type' ";
+	// 	if(empty($id)){
+	// 		$save = $this->db->query("INSERT INTO users set ".$data);
+	// 	}else{
+	// 		$save = $this->db->query("UPDATE users set ".$data." where id = ".$id);
+	// 	}
+	// 	if($save){
+	// 		return 1;
+	// 	}
+	// }
 	
 	function delete_user(){
 		extract($_POST);
